d33e56895d558d571872755849bd16d5dd6d747f
Arthur <arthurchagas01@gmail.com>
2023-10-04 08:38:55 -0300

    Content created

---
+++ src/app/data/dataFake.ts
@@ -0,0 +1,26 @@
+export const dataFake = [
+  {
+    "id": "1",
+    "title": "NOVO HOMEM DE FERRO ANUNCIADO",
+    "description": "Marvel Studios anuncia novo filme do Homem de Ferro com um novo ator.",
+    "photoCover": "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTqFTnUZhP5BoG2JeY2fYo2ypFduthqT_VQNQ&usqp=CAU"
+  },
+  {
+    "id": "2",
+    "title": "Nova série do Universo Marvel anunciada",
+    "description": "Disney, dona da Marvel, anuncia nova série continuação de Homem Aranha focada no universo do Demolidor e que unirá diversos personagens da franquia.",
+    "photoCover": "https://legadodamarvel.com.br/wp-content/uploads/2022/02/series-originais-da-marvel-na-netflix-podem-estar-deixando-a-plataforma-legadodamarvel.jpg"
+  },
+  {
+    "id": "3",
+    "title": "Ashoka Tano: Conheça a nova heroína Jedi",
+    "description": "Conheça ainda mais sobre a Ahsoka e entenda o motivo de ela ser a principal personagem da saga atual de Star Wars.",
+    "photoCover": "https://cdn.ome.lt/1UJHe3z6wfxr7ULCwSzr_cJ760E=/1200x630/smart/extras/conteudos/ahsoka-audiencia-estreia.jpg"
+  },
+  {
+    "id": "4",
+    "title": "Veja as primeiras imagens do filme do Aquaman",
+    "description": "Veja as primeiras imagens do novo filme do Aquaman que estreará em Dezembro nos cinemas.",
+    "photoCover": "https://sm.ign.com/t/ign_br/preview/a/aquaman-mo/aquaman-movie-footage-showcased-ocean-masters-armada_2teq.1200.jpg"
+  }
+]
